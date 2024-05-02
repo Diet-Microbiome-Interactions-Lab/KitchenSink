b1854e96cba6fe5ec3cf364694c7dc4b37a11eca
Marcelo Guerrero <guerrero@pal-10-184-53-62.itap.purdue.edu>
2024-05-02 14:51:27 -0400

    Added a script to calculate C1V1_C1V2 formula.

---
+++ solution_dilution.py
@@ -0,0 +1,12 @@
+import fire
+def solution_dilution(initial_concentration, final_concentration, final_volume):
+    initial_concentration = float(initial_concentration)
+    final_concentration = float(final_concentration)
+    final_volume = float(final_volume)
+    volume_to_add = final_concentration * final_volume / initial_concentration
+    difference = final_volume - volume_to_add
+    return(
+        "Solvent Volume: " + str(difference) +
+        "\nVolume to add: " + str(volume_to_add))
+if __name__ == '__main__':
+    fire.Fire(solution_dilution)
